907e5428c21273602a753d1c7154d4f012ead6f6
Wasif007 <wasifateeq007@hotmail.com>
2023-08-10 23:52:40 +0500

    Styling of front page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Header from './Components/Header';
+import Home from './Components/Home';
 
 function App() {
   return (
     <div>
      <Header/>
+     <Home/>
     </div>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='container'>
+            <form>
+                <input placeholder='Title'></input>
+                <textarea placeholder='Description'></textarea>
+                <button>ADD</button>
+            </form>
+      
+    </div>
+  )
+}
+
+export default Home
