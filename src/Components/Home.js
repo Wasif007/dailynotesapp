34770acd70867ea7548c5a2cb7f7040354a1aba7
Wasif007 <wasifateeq007@hotmail.com>
2023-08-13 11:36:44 +0500

    Refactoring

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,38 +2,30 @@ import React, { useState } from 'react'
 import Tasks from './Tasks'
 
 const Home = () => {
-  const [titleSetting,setTitleSetting]=useState([]);
+  const [objectArray,setObjectArray]=useState([]);
   const [title,setTitle]=useState("");
   const [description,setDescription]=useState("");
-const onSubmitHandler=(e)=>{
-e.preventDefault();
-setTitleSetting([...titleSetting,{title,description}]);
-}
-const deleteArray=(index)=>{
-const filteredArray=titleSetting.filter((val,i)=>{
-  return i!==index;
-})
-setTitleSetting(filteredArray);
-}
+  const onSubmitHandler=(e)=>{
+    e.preventDefault();
+    setObjectArray([...objectArray,{title,description}]);
+  }
   return (
-    <div className='container'>
-      <h1>DAILY TASKS</h1>
-            <form onSubmit={onSubmitHandler}>
-                <input placeholder='Title' value={title} onChange={(e)=>{
-                  setTitle(e.target.value);
-                }}></input>
-                <textarea placeholder='Description' value={description} onChange={(e)=>{
-                  setDescription(e.target.value);
+   <div className='container'>
+    <form onSubmit={onSubmitHandler}>
+      <input placeholder='Title' value={title} onChange={(e)=>{
+        setTitle(e.target.value);
+      }}></input>
+      <textarea placeholder='Description' value={description} onChange={(e)=>{
+        setDescription(e.target.value);
+      }}></textarea>
+      <button>ADD</button>
+    </form>
+    {
+      objectArray.map((item,index)=>
+      (<Tasks key={index} title={item.title} description={item.description}/>))
+    }
 
-                }}></textarea>
-                <button>ADD</button>
-            </form>
-            {titleSetting.map((item,index)=>(
-              <Tasks key={index} title={item.title} description={item.description} deleteArray={deleteArray} index={index}/>
-            ))}
-            
-      
-    </div>
+   </div>
   )
 }
 
--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -1,17 +1,15 @@
 import React from 'react'
 
 const Tasks = (props) => {
-    const {title,description,deleteArray,index}=props;
+    const {title,description}=props;
   return (
+    
     <div className='tasks'>
-        <div>
-            <p>{title}</p>
-        <span>{description}</span>
-        </div>
-        
-        <button onClick={()=>{
-            deleteArray(index);
-        }}>-</button>
+      <div>
+      <p>{title}</p>
+      <span>{description}</span>
+    </div>
+    <button>-</button>
     </div>
   )
 }
